601bddeffa46bdc9c94a84dc92e759595a19a34d
bakcoder <gudwls45@gmail.com>
2021-04-01 00:43:04 +0900

    MODIFY: add bodyparser & nodemon

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json())
 
 // MongoDB connect
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://bakcoder:abcd@bakcodercluster.cvjhw.mongodb.net/bakcoderDB?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World');
+})
+
+app.post('/register', (req, res) => {
+    //회원 가입 시 필요한 정보들을 클라이언트에서 가져와 DB로 insert
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
-    console.log(`listening http://localhost:${port}`)
+    console.log(`listening http://localhost:${port}`);
 })
