c10822c61963c74e226964d657a41f68800f05f3
bakcoder <gudwls45@gmail.com>
2021-04-22 23:49:15 +0900

    ADD: redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import rootReducer from './_reducers/index';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSTION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSTION__()
+      )}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
